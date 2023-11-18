5848b37b817f57dd4247ad0937f857e4d7bd9f8b
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-11-18 22:40:02 +0000

    feat: add Tabulus storybook file

---
+++ packages/tabulus/src/components/Tabulus/Tabulus.stories.tsx
@@ -0,0 +1,30 @@
+import { Tabulus } from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  component: Tabulus,
+  tags: ['autodocs'],
+  title: 'Tabulus',
+} satisfies Meta<typeof Tabulus>;
+
+type Story = StoryObj<typeof meta>;
+
+const columns = [
+  { id: 'id', title: 'ID' },
+  { id: 'name', title: 'Name' },
+  { id: 'age', title: 'Age' },
+  { id: 'title', title: 'Title' },
+];
+
+const data = [
+  { id: 1, name: 'Steve', age: 23, title: '' },
+  { id: 2, name: 'Gemma', age: 33, title: '' },
+  { id: 3, name: 'Tet', age: 36, title: '' },
+];
+
+/** Address component containing all available data. */
+const Default: Story = { args: { columns, data, id: 'test' } };
+
+export default meta;
+export { Default };
