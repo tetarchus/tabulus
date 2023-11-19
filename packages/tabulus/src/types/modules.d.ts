9349e55cccba3df20a21e021280cc08b902c2ac6
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-11-19 21:36:01 +0000

    chore: sort out module types

--- packages/tabulus/src/types/modules.d.ts
+++ packages/tabulus/src/types/modules.d.ts
@@ -1,4 +1,13 @@
+import '@emotion/react';
+
 import type { AxeMatchers } from 'vitest-axe/matchers';
+
+/**
+ * Inject custom theme into @emotion to allow auto-complete for theme properties.
+ */
+declare module '@emotion/react' {
+  export { Theme } from '@tabulus/types/theme';
+}
 
 /**
  * Extended types for vitest. Not used in the library itself.
