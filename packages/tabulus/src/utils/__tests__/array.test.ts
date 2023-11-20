87e9b28a91c3dcf9f44bf230c32412d17f1be31d
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-11-20 01:38:00 +0000

    fix: remove `todo` on written tests

--- packages/tabulus/src/utils/__tests__/array.test.ts
+++ packages/tabulus/src/utils/__tests__/array.test.ts
@@ -4,13 +4,13 @@ import { arrayIncludes } from '../array';
 
 describe('Array Util Tests', () => {
   describe('Tests for `arrayIncludes`', () => {
-    it.todo('should return `true` when the value is included in the array', () => {
+    it('should return `true` when the value is included in the array', () => {
       expect.assertions(1);
       const result = arrayIncludes(['test', 'values'], 'test');
       expect(result).toBeTruthy();
     });
 
-    it.todo('should return `false` when the value is not included in the array', () => {
+    it('should return `false` when the value is not included in the array', () => {
       expect.assertions(1);
       const result = arrayIncludes(['test', 'values'], 'new');
       expect(result).toBeFalsy();
