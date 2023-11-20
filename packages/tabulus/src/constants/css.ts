9706354c35e93a1db4d44af8aaebecf20537360b
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-11-20 01:29:21 +0000

    feat: add CSS value constants

---
+++ packages/tabulus/src/constants/css.ts
@@ -0,0 +1,11 @@
+const CSS_VALUES = {
+  FLEX: {
+    ALIGN: {
+      START: 'flex-start',
+      END: 'flex-end',
+      CENTER: 'center',
+    },
+  },
+};
+
+export { CSS_VALUES };
--- packages/tabulus/src/constants/index.ts
+++ packages/tabulus/src/constants/index.ts
@@ -1,3 +1,4 @@
 export { CLASSES } from './classes';
+export { CSS_VALUES } from './css';
 export { ROLES } from './roles';
 export { LIBRARY_NAME, PREFIX } from './tabulus';
