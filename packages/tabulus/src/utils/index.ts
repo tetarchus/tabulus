fe0ce9337f6fa08db6d92e3e23d00f9a00617768
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-11-27 01:35:14 +0000

    chore: remove utils covered by `@tetarchus/utils`

--- packages/tabulus/src/utils/index.ts
+++ packages/tabulus/src/utils/index.ts
@@ -1,14 +1,5 @@
-export { arrayIncludes } from './array';
 export { getHorizontalAlignProperty, getVerticalAlignProperty } from './cell';
 export { validateColumnDefinition, validateColumnDefinitions } from './columns';
-export {
-  cleanObject,
-  fromEntries,
-  fullClean,
-  objectEntries,
-  objectKeys,
-  swapKeyValue,
-} from './object';
 export { createTableOptions } from './options';
 export { WARNINGS } from './warnings';
 export { styled, ThemeProvider } from './style';
