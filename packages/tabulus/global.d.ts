070bbcbcb2b9a7231b868bc6994c0ecdb982472f
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-11-29 20:53:08 +0000

    chore: remove global.d.ts and move contents

--- packages/tabulus/global.d.ts
+++
@@ -1,8 +0,0 @@
-import '@emotion/react';
-
-/**
- * Inject custom theme into @emotion to allow auto-complete for theme properties.
- */
-declare module '@emotion/react' {
-  export { Theme } from '@tabulus/types';
-}
