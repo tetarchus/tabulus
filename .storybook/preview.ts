46a3cfe8ecc5b6fb7c2f0c6b79ee39c25bc20c23
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-12-06 01:33:40 +0000

    fix: resolve eslint issues

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,8 +5,8 @@ const storybookPreviewBase: Preview = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+        color: /(background|color)$/iu,
+        date: /date$/iu,
       },
     },
   },
