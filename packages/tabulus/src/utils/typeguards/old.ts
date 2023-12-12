3e53fadf88b21e5cd80bb6e8d95df1e94db99827
tetarchus <8436118+tetarchus@users.noreply.github.com>
2023-12-12 00:39:18 +0000

    chore: storage file for typeguards that aren't currently being used but may be required.

---
+++ packages/tabulus/src/utils/typeguards/old.ts
@@ -0,0 +1,14 @@
+// TODO: Remove these if not required
+
+// const isDefinedRef = <T>(ref: MutableRefObject<T | undefined>): ref is MutableRefObject<T> =>
+//   ref.current != null;
+
+// const isHeaderRow = <RowData extends SimpleRowData>(
+//   row: RowComponent<RowData>,
+// ): row is RowComponentHeader & RowComponentBase<RowData> => row.type === 'header';
+
+// const isTableRow = <RowData extends SimpleRowData>(
+//   row: RowComponent<RowData>,
+// ): row is RowComponentStandard<RowData> & RowComponentBase<RowData> => row.type === 'table';
+
+export {};
